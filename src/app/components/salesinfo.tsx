ec9d494fa022ed13a7e0f985570c335311412537
Thuan Silva <thuansilva14@gmail.com>
2025-10-08 02:13:00 -0400

    fix: Adicionar Responsividade

--- src/app/components/salesinfo.tsx
+++ src/app/components/salesinfo.tsx
@@ -26,34 +26,35 @@ export const SalesInfo: React.FC = () => {
     { value: unitsData.available },
   ];
 
-  const chartColors: string[] = ["#9C000D", "#C30010", "#F3F4F6"];
+  const chartColors: string[] = ["#9C000D", "#C30010", "#F8F4F2"];
 
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-      {/* Card de Unidades */}
-      <div className="bg-white p-4 sm:p-6 rounded-xl border border-gray-200 shadow-sm flex flex-col sm:flex-row items-center justify-around">
-        <div className="relative">
+    <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+      <div className="bg-white py-[34px] pr-12 sm:p-6 rounded-xl border border-gray-light shadow-sm flex flex-col sm:flex-row items-center ">
+        <div className="relative ">
           <MultiSegmentDonutChart
             data={chartData}
             colors={chartColors}
-            size={150}
+            size={200}
             strokeWidth={20}
           />
-          <div className="absolute inset-0 flex flex-col items-center justify-center text-center">
-            <span className="text-xs sm:text-sm text-gray-600">Unidades</span>
-            <span className="text-2xl sm:text-3xl font-bold text-gray-800">
+          <div className="absolute  inset-0 flex flex-col items-center justify-center text-center">
+            <span className="text-sm sm:text-sm font-medium text-gray-medium-dark ">
+              Unidades
+            </span>
+            <span className="text-2xl sm:text-3xl font-semibold text-gray-dark">
               {unitsData.total}
             </span>
           </div>
         </div>
 
-        <div className="mt-4 sm:mt-0 sm:ml-8 flex flex-col space-y-3">
+        <div className="mt-4 sm:mt-0 sm:ml-20 flex flex-col space-y-3 text-sm-medium">
           {[
             {
               label: "Disponíveis",
               value: unitsData.available,
               color: "#403E3D",
-              dot: "bg-gray-300",
+              dot: "#f2f1f0",
             },
             {
               label: "Reservados",
@@ -64,23 +65,23 @@ export const SalesInfo: React.FC = () => {
             {
               label: "Vendidos",
               value: unitsData.sold,
-              color: "#403E3D",
+              color: "#403e3d",
               dot: "#9C000D",
             },
           ].map((item, idx) => (
-            <div key={idx} className="flex items-start space-x-3">
+            <div key={idx} className=" h-[55px] flex items-start space-x-3 ">
               <span
                 className={`w-2 h-2 rounded-full mt-[9px]`}
                 style={{ backgroundColor: item.dot }}
               ></span>
               <div>
                 <p
-                  className="text-xs sm:text-sm font-medium leading-5"
+                  className="text-sm-medium text-gray-medium-dark "
                   style={{ color: item.color }}
                 >
                   {item.label}
                 </p>
-                <p className="font-bold text-lg sm:text-xl text-gray-800">
+                <p className=" display-xs-semibold text-gray-dark">
                   {item.value}
                 </p>
               </div>
@@ -89,21 +90,20 @@ export const SalesInfo: React.FC = () => {
         </div>
       </div>
 
-      {/* Card de Total Vendido */}
-      <div className="bg-white p-4 sm:p-6 rounded-xl border border-gray-200 shadow-sm flex flex-col sm:flex-row items-center justify-center sm:space-x-8">
+      <div className="bg-white py-[34px] pr-12 sm:p-6 rounded-xl border border-gray-light shadow-sm flex flex-col sm:flex-row items-center  ">
         <div className="relative mb-4 sm:mb-0">
           <MultiSegmentDonutChart
             data={[{ value: 80 }, { value: 20 }]}
             colors={["#9C000D", "#F3F4F6"]}
-            size={150}
+            size={200}
             strokeWidth={20}
           />
         </div>
-        <div className="text-left">
-          <p className="text-xs sm:text-sm text-gray-600">Total Vendido</p>
-          <p className="text-3xl sm:text-5xl font-bold text-gray-800 mt-1">
-            80%
+        <div className="text-center sm:text-left sm:ml-20">
+          <p className=" text-sm-medium text-neutral-dark-gray">
+            Total Vendido
           </p>
+          <p className=" display-lg-semibold  text-gray-dark mt-1">80%</p>
         </div>
       </div>
     </div>
