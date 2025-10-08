0b970eb80b023e0579e649c32c878e5820f678bd
Thuan Silva <thuansilva14@gmail.com>
2025-10-07 21:39:00 -0400

    fix: Atualiza estilos do cabeçalho para usar classes de estilo e cores definidas

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -2,13 +2,18 @@ import { CompassIcon } from "../styles/icons";
 
 const Header = () => (
   <div className="flex flex-col sm:flex-row sm:justify-between sm:items-center gap-2 sm:gap-0">
-    <h1 className="text-2xl font-bold leading-8 text-gray-800">
+    <h1 className="text-2xl font-bold leading-8 text-gray-dark">
       Splendor Jardim
     </h1>
 
     <div
-      className="flex items-center text-sm font-semibold px-4 py-1.5 rounded-full mt-2 sm:mt-0"
-      style={{ backgroundColor: "#FFF2DE", color: "#9A3412" }}
+      className="flex 
+      items-center 
+      text-sm 
+      font-semibold px-4 py-1.5 rounded-full mt-2 sm:mt-0 
+      bg-alert-surface
+      text-alert-dark
+      "
     >
       Em Construção
       <CompassIcon className="w-4 h-4 ml-2" />
