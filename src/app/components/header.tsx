cf419fedc7d4ac6876b3c376504614df3f377be2
Thuan Silva <thuansilva14@gmail.com>
2025-10-08 01:54:13 -0400

    feat: Ajuste de layout

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -1,16 +1,14 @@
 import { CompassIcon } from "../styles/icons";
 
 const Header = () => (
-  <div className="flex flex-col sm:flex-row sm:justify-between sm:items-center gap-2 sm:gap-0">
-    <h1 className="text-2xl font-bold leading-8 text-gray-dark">
-      Splendor Jardim
-    </h1>
+  <div className="flex flex-wrap justify-between items-center gap-4">
+    <h1 className="text-gray-dark heading-lg">Splendor Jardim</h1>
 
     <div
       className="flex 
       items-center 
       text-sm 
-      font-semibold px-4 py-1.5 rounded-full mt-2 sm:mt-0 
+      font-semibold px-4 py-1.5 rounded-full
       bg-alert-surface
       text-alert-dark
       "
